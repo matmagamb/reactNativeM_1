c08ba032b921abd2cc87ba791d0699f8172f87fb
Matheus Ambrosio <ceifa13@outlook.com>
2023-01-09 17:48:27 -0300

    estilizando elemnetos 5:07

--- App.tsx
+++ App.tsx
@@ -2,8 +2,20 @@ import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello</Text>
+    <View
+      style={{
+        backgroundColor: "#131016",
+        flex: 1,
+      }}
+    >
+      <Text
+        style={{
+          color: "#fff",
+          fontSize
+        }}
+      >
+        Hello
+      </Text>
       <Text>Hello</Text>
     </View>
   );
