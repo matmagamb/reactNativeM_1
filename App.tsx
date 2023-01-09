648a445560cc3b763022198650563480fe439d25
Matheus Ambrosio <ceifa13@outlook.com>
2023-01-09 17:26:45 -0300

    entendendo jsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your ap!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
